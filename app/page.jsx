5c87d044709fc3622c0eda08614b0315d88a6327
Marco Antonio Butrico <marcobutrico@gmail.com>
2024-06-02 12:40:57 -0300

    refact: entire project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vero est debitis saepe cumque ratione, eligendi ad. Corporis, saepe magnam minima distinctio, praesentium mollitia quia voluptatem neque blanditiis voluptatum iure!</span>
+    </div>  
+    )
+}
+
+export default HomePage
